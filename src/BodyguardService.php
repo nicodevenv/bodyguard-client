f6610e65d30f268697e847b2adfa5472237d8f34
Nicolas CHUNG <nico.dev.env@gmail.com>
2019-03-24 14:24:22 +0100

    fix

--- src/BodyguardService.php
+++ src/BodyguardService.php
@@ -1,157 +1,166 @@
 <?php
 
-    namespace BodyguardClient;
+	namespace BodyguardClient;
 
-    use GuzzleHttp\Exception\GuzzleException;
-    use Symfony\Component\HttpFoundation\Request;
+	use GuzzleHttp\Exception\GuzzleException;
+	use Symfony\Component\HttpFoundation\Request;
 
-    class BodyguardService
-    {
-        /** @var string */
-        protected $applicationId;
+	class BodyguardService
+	{
+		/** @var string */
+		protected $applicationId;
 
-        /** @var string */
-        protected $applicationSecret;
+		/** @var string */
+		protected $applicationSecret;
 
-        /** @var string */
-        protected $baseUrl;
+		/** @var string */
+		protected $baseUrl;
 
-        public function __construct(
-            string $applicationId,
-            string $applicationSecret,
-            string $bodyguardBaseUrl
-        ) {
-            $this->baseUrl = $bodyguardBaseUrl;
-            $this->applicationId = $applicationId;
-            $this->applicationSecret = $applicationSecret;
-        }
+		public function __construct(
+			string $applicationId,
+			string $applicationSecret,
+			string $bodyguardBaseUrl
+		)
+		{
+			$this->baseUrl           = $bodyguardBaseUrl;
+			$this->applicationId     = $applicationId;
+			$this->applicationSecret = $applicationSecret;
+		}
 
-        public function register(Request $request, array $data): array
-        {
-            $bodyguardClient = $this->getBodyguardClient($request);
+		public function register(Request $request, array $data): array
+		{
+			$bodyguardClient = $this->getBodyguardClient($request);
 
-            try {
-                return json_decode($bodyguardClient->registerUser(
-                    $data['username'],
-                    $data['email'],
-                    $data['password']
-                )->getBody()->getContents(), true);
-            } catch (BodyguardException $e) {
-                return $this->getBodyguardException($e);
-            } catch (GuzzleException $e) {
-                return $this->getBodyguardException($e);
-            }
-        }
+			try {
+				return json_decode($bodyguardClient->registerUser(
+					$data['username'],
+					$data['email'],
+					$data['password']
+				)->getBody()->getContents(), true);
+			} catch (BodyguardException $e) {
+				return $this->getBodyguardException($e);
+			} catch (GuzzleException $e) {
+				return $this->getBodyguardException($e);
+			}
+		}
 
-        public function login(Request $request, array $data): array
-        {
-            $bodyguardClient = $this->getBodyguardClient($request);
+		public function login(Request $request, array $data): array
+		{
+			$bodyguardClient = $this->getBodyguardClient($request);
 
-            try {
-                return json_decode($bodyguardClient->loginUser(
-                    $data['email'],
-                    $data['password']
-                )->getBody()->getContents(), true);
-            } catch (BodyguardException $e) {
-                return $this->getBodyguardException($e);
-            } catch (GuzzleException $e) {
-                return $this->getBodyguardException($e);
-            }
-        }
+			try {
+				return json_decode($bodyguardClient->loginUser(
+					$data['email'],
+					$data['password']
+				)->getBody()->getContents(), true);
+			} catch (BodyguardException $e) {
+				return $this->getBodyguardException($e);
+			} catch (GuzzleException $e) {
+				return $this->getBodyguardException($e);
+			}
+		}
 
-        public function getToken(Request $request, array $data): array
-        {
-            $bodyguardClient = $this->getBodyguardClient($request);
+		public function getToken(Request $request, array $data): array
+		{
+			$bodyguardClient = $this->getBodyguardClient($request);
 
-            try {
-                return json_decode($bodyguardClient->getUserSessionToken(
-                    $data['user_id'],
-                    $data['user_secret']
-                )->getBody()->getContents(), true);
-            } catch (BodyguardException $e) {
-                return $this->getBodyguardException($e);
-            } catch (GuzzleException $e) {
-                return $this->getBodyguardException($e);
-            }
-        }
+			try {
+				return json_decode($bodyguardClient->getUserSessionToken(
+					$data['user_id'],
+					$data['user_secret']
+				)->getBody()->getContents(), true);
+			} catch (BodyguardException $e) {
+				return $this->getBodyguardException($e);
+			} catch (GuzzleException $e) {
+				return $this->getBodyguardException($e);
+			}
+		}
 
-        public function validateToken(Request $request): array
-        {
-            $bodyguardClient = $this->getBodyguardClient($request);
+		public function validateToken(Request $request): array
+		{
+			if (null === $request->headers->get('authorization', null)) {
+				return ['code' => 200];
+			}
 
-            try {
-                return json_decode($bodyguardClient->validateToken(
-                    $request->headers->get('authorization')
-                )->getBody()->getContents(), true);
-            } catch (BodyguardException $e) {
-                return $this->getBodyguardException($e);
-            } catch (GuzzleException $e) {
-                return $this->getBodyguardException($e);
-            }
-        }
+			$bodyguardClient = $this->getBodyguardClient($request);
 
-        public function logout(Request $request): array
-        {
-            $bodyguardClient = $this->getBodyguardClient($request);
+			try {
+				return json_decode($bodyguardClient->validateToken(
+					$request->headers->get('authorization')
+				)->getBody()->getContents(), true);
+			} catch (BodyguardException $e) {
+				return $this->getBodyguardException($e);
+			} catch (GuzzleException $e) {
+				return $this->getBodyguardException($e);
+			}
+		}
 
-            try {
-                return json_decode($bodyguardClient->logout(
-                    $request->headers->get('authorization')
-                )->getBody()->getContents(), true);
-            } catch (BodyguardException $e) {
-                return $this->getBodyguardException($e);
-            } catch (GuzzleException $e) {
-                return $this->getBodyguardException($e);
-            }
-        }
+		public function logout(Request $request): array
+		{
+			if (null === $request->headers->get('authorization', null)) {
+				return ['code' => 200];
+			}
 
-        /**
-         * @param BodyguardException|GuzzleException $exception
-         *
-         * @return array
-         */
-        public function getBodyguardException($exception): array
-        {
-            return [
-                'status' => $exception->getCode(),
-                'message' => $exception->getMessage()
-            ];
-        }
+			$bodyguardClient = $this->getBodyguardClient($request);
 
-        /**
-         * @param Request $request
-         * @param bool    $strict
-         *
-         * @return null|string
-         * @throws BodyguardException
-         */
-        public function getUserId(Request $request, bool $strict = true): ?string
-        {
-            $tokenValidation = null;
+			try {
+				return json_decode($bodyguardClient->logout(
+					$request->headers->get('authorization')
+				)->getBody()->getContents(), true);
+			} catch (BodyguardException $e) {
+				return $this->getBodyguardException($e);
+			} catch (GuzzleException $e) {
+				return $this->getBodyguardException($e);
+			}
+		}
 
-            if ($request->headers->get('authorization')) {
-                $tokenValidation = $this->validateToken($request);
+		/**
+		 * @param BodyguardException|GuzzleException $exception
+		 *
+		 * @return array
+		 */
+		public function getBodyguardException($exception): array
+		{
+			return [
+				'status'  => $exception->getCode(),
+				'message' => $exception->getMessage(),
+			];
+		}
 
-                if ($strict && !isset($tokenValidation['data']['user_data']['user_id'])) {
-                    throw new BodyguardException('403', 'Invalid token', 'User id not found in token validation request');
-                }
-            }
+		/**
+		 * @param Request $request
+		 * @param bool    $strict
+		 *
+		 * @return null|string
+		 * @throws BodyguardException
+		 */
+		public function getUserId(Request $request, bool $strict = true): ?string
+		{
+			$tokenValidation = null;
 
-            if (null !== $tokenValidation){
-                return $tokenValidation['data']['user_data']['user_id'];
-            }
+			if ($request->headers->get('authorization')) {
+				$tokenValidation = $this->validateToken($request);
 
-            return null;
-        }
+				if ($strict && !isset($tokenValidation['data']['user_data']['user_id'])) {
+					throw new BodyguardException('403', 'Invalid token', 'User id not found in token validation request');
+				}
+			}
 
-        private function getBodyguardClient(Request $request): BodyguardClient
-        {
-            return new BodyguardClient(
-                $this->baseUrl,
-                $this->applicationId,
-                $this->applicationSecret,
-                $request->getClientIp(),
-                $request->headers->get('user-agent')
-            );
-        }
-    }
+			if (null !== $tokenValidation) {
+				return $tokenValidation['data']['user_data']['user_id'];
+			}
+
+			return null;
+		}
+
+		private function getBodyguardClient(Request $request): BodyguardClient
+		{
+			return new BodyguardClient(
+				$this->baseUrl,
+				$this->applicationId,
+				$this->applicationSecret,
+				$request->getClientIp(),
+				$request->headers->get('user-agent')
+			);
+		}
+	}
